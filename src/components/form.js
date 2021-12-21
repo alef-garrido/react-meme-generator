6736058195ae21d57456fa81b288faadd2afd20d
alef-garrido <oa.g11@icloud.com>
2021-12-21 13:16:13 -0600

    Add Form Button

---
+++ src/components/form.js
@@ -0,0 +1,12 @@
+export default function Form() {
+  return (
+    <div>
+      <form className="textInputs">
+        < input type="text" name="text1" placeholder="Shut up"/>
+        < input type="text" name="text2" placeholder="and take my money"/>
+      </form>
+      <button>Get me a new Meme 🖼️</button>
+
+    </div>
+  )
+}
--- src/components/page.js
+++ src/components/page.js
@@ -1,9 +1,11 @@
 import Navbar from "./navbar";
+import Form from "./form";
 
 export default function Page() {
   return (
     <div className="page">
       <Navbar />
+      <Form />
     </div>
   )
 }
