4104c2cbd84cc4303d1e8e87c9cb5e536b4302fc
alef-garrido <oa.g11@icloud.com>
2021-12-22 14:47:52 -0600

    Add react state for image container

--- src/components/form.js
+++ src/components/form.js
@@ -1,8 +1,14 @@
+import React from 'react';
+import memesData from "../memesData"
+
 export default function Form() {
+  const [memeImage, setImage] = React.useState('') 
   function myOnClick() {
-    return(
-      console.log('hey kittie gurl!')
-    )
+    const memeArr = memesData.data.memes
+    const randomN = Math.floor(Math.random() * memeArr.length)
+    let url = memeArr[randomN].url
+
+    setImage(url)
   }
   return (
     <main>
@@ -10,6 +16,9 @@ export default function Form() {
         < input type="text" name="text1" placeholder="Top text"/>
         < input type="text" name="text2" placeholder="Bottom text"/>
         <button onClick={myOnClick}>Get me a new Meme 🖼️</button>
+        <div className="imageContainer">
+          <img src={memeImage}  alt="meme image"/>
+        </div>
       </div>
     </main>
   )
